656de924a91d57d8f2625fc8e7f50e579973f838
JeremieAgee <you@example.com>
2023-12-12 20:44:52 -0600

    added card pics and updated nav

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+let rectangle = document.createElement("div");
+rectangle.className = "rectangle";
+for (let i = 0; i < 6; i++) {
+	let side = document.createElement("div");
+	side.className = "face face" + i;
+    if(i<4){
+        side.innerHTML = "Jeremie";
+    }
+    rectangle.appendChild(side);
+}
+document.body.appendChild(rectangle);
